04f49e9e6448a01c87ef3dd085618a923360c7c7
zulolo <dodoleon@sina.com>
2016-08-17 22:11:10 +0800

    Everything is working

--- src/iot10086publish.c
+++ src/iot10086publish.c
@@ -12,10 +12,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
-#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <linux/types.h>
 #include <stddef.h>
 #include <signal.h>
+#include <string.h>
 #include <curl/curl.h>
 
 #define __USED_IOT10086PUBLISH_INTERNAL__
@@ -35,45 +42,30 @@ int32_t nSendDataToNRF905Socket(int32_t nConnectedSocketFD, nRF905CommTask_t* pN
 	return send(nConnectedSocketFD, unBuffer, sizeof(nRF905CommTask_t) + NRF905_TX_PAYLOAD_LEN, 0);
 }
 
-int32_t nUnixSocketConn(const char *pNRF905ServerName)
+/* Create a client endpoint32_t and connect to a server.   Returns fd if all OK, <0 on error. */
+int32_t nTCPSocketConn(const char* pServerIPAddr, uint16_t unServerPort)
 {
-	int32_t nConnectSocketFd;
-	int32_t nLen;
-	struct sockaddr_un tSocketAddrUn;
+	int32_t nConnectSocket;
+	struct sockaddr_in tConnSocketAddr;
 
-	printf("Try to connect server socket.\n");
-	if ((nConnectSocketFd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0){     /* create a UNIX domain stream socket */
-		printf("Create UNIX socket failed with error %d.\n", errno);
-		NRF905D_LOG_ERR("Create UNIX socket failed with error %d.", errno);
+    if ((nConnectSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    	NRF905D_LOG_ERR("Failed to create socket with error %d.", errno);
 		return(-1);
-	}
-	memset(&tSocketAddrUn, 0, sizeof(tSocketAddrUn));            /* fill socket address structure with our address */
-	tSocketAddrUn.sun_family = AF_UNIX;
-	sprintf(tSocketAddrUn.sun_path, "scktmp%05d", getpid());
-	nLen = offsetof(struct sockaddr_un, sun_path) + strlen(tSocketAddrUn.sun_path);
-	printf("Try to unlink sun_path.\n");
-	unlink(tSocketAddrUn.sun_path);               /* in case it already exists */
-	printf("Ulink tSocketAddrUn.sun_path OK.\n");
-	if (bind(nConnectSocketFd, (struct sockaddr *)&tSocketAddrUn, nLen) < 0) {
-		close(nConnectSocketFd);
-		printf("Bind socket failed with error %d.", errno);
-		NRF905D_LOG_ERR("Bind socket failed during client connect with error %d.", errno);
-		return (-1);
-	}else{
-		/* fill socket address structure with server's address */
-		memset(&tSocketAddrUn, 0, sizeof(tSocketAddrUn));
-		tSocketAddrUn.sun_family = AF_UNIX;
-		strcpy(tSocketAddrUn.sun_path, pNRF905ServerName);
-		nLen = offsetof(struct sockaddr_un, sun_path) + strlen(pNRF905ServerName);
-		if (connect(nConnectSocketFd, (struct sockaddr *)&tSocketAddrUn, nLen) < 0)   {
-			close(nConnectSocketFd);
-			printf("Connect server failed with error %d.", errno);
-			NRF905D_LOG_ERR("Connect server failed with error %d.", errno);
-			return (-1);
-		}else{
-			return (nConnectSocketFd);
-		}
-	}
+    }
+
+    /* Construct the server sockaddr_in structure */
+    memset(&tConnSocketAddr, 0, sizeof(tConnSocketAddr));       /* Clear struct */
+    tConnSocketAddr.sin_family = AF_INET;                  /* Internet/IP */
+    tConnSocketAddr.sin_addr.s_addr = inet_addr(pServerIPAddr);  /* IP address */
+    tConnSocketAddr.sin_port = unServerPort;       /* server port */
+    /* Establish connection */
+    if (connect(nConnectSocket, (struct sockaddr *) &tConnSocketAddr, sizeof(tConnSocketAddr)) < 0) {
+    	close(nConnectSocket);
+    	NRF905D_LOG_ERR("Failed to connect with server with error %d.", errno);
+		return(-1);
+    }
+
+    return nConnectSocket;
 }
 
 int main(void) {
@@ -89,12 +81,13 @@ int main(void) {
 	int32_t nConnectedSocketFd;
 	int32_t nReceivedCNT;
 	SENSOR_DATA_T tSensorData;
+//	uint32_t unIndex;
 
     tSignalINT_Action.sa_sigaction = sigINT_Handler;
     tSignalINT_Action.sa_flags = SA_SIGINFO;
     sigaction(SIGINT, &tSignalINT_Action, NULL);
 
-	nConnectedSocketFd = nUnixSocketConn(NRF905_SERVER_NAME);
+	nConnectedSocketFd = nTCPSocketConn(LOCAL_HOST_IP, NRF905_SERVER_PORT);
 	if(nConnectedSocketFd < 0)
 	{
 		printf("Error[%d] when connecting...", errno);
@@ -104,6 +97,23 @@ int main(void) {
 	tNRF905CommTask.tCommType = NRF905_COMM_TYPE_TX_PKG;
 	tNRF905CommTask.unCommByteNum = NRF905_TX_PAYLOAD_LEN;
 
+	/* get a curl handle */
+	pCurl = curl_easy_init();
+	if (NULL == pCurl){
+		NRF905D_LOG_ERR("Initialize curl error.");
+		close(nConnectedSocketFd);
+		return (-1);
+	}
+
+	/* First set the URL that is about to receive our POST. This URL can
+	just as well be a https:// URL if that is what should receive the
+	data. */
+	curl_easy_setopt(pCurl, CURLOPT_VERBOSE, 1L);	// Show debug info
+	curl_easy_setopt(pCurl, CURLOPT_URL, DEVICE_API_URL);
+	pHttpHeaders = NULL;
+	pHttpHeaders = curl_slist_append(pHttpHeaders, "Content-Type: application/json");
+	pHttpHeaders = curl_slist_append(pHttpHeaders, "api-key:PPsmfd=F0gDlwdZLgGsrVEAsIeY=");
+	curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, pHttpHeaders);
 
 	while (NRF905_FALSE == unNeedtoClose){
 		if (nSendDataToNRF905Socket(nConnectedSocketFd, &tNRF905CommTask, unACK_TX_Payload) < 0 ){
@@ -114,44 +124,35 @@ int main(void) {
 				printf("Receive from unix domain socket error with code:%d.", errno);
 				NRF905D_LOG_ERR("Receive from unix domain socket error with code:%d.", errno);
 			} else {
-				printf("Socket %d received one message from nRF905 server.\n", nConnectedSocketFd);
+//				printf("Socket %d received one message from nRF905 server.\n", nConnectedSocketFd);
+//				for (unIndex = 0; unIndex < ARRAY_SIZE(unACK_RX_Payload); unIndex++){
+//					printf("0x%02X\n", unACK_RX_Payload[unIndex]);
+//				}
 				memcpy(&tSensorData, unACK_RX_Payload + 1, sizeof(tSensorData));
-				/* get a curl handle */
-				pCurl = curl_easy_init();
-				if(pCurl) {
-					/* First set the URL that is about to receive our POST. This URL can
-					just as well be a https:// URL if that is what should receive the
-					data. */
-					curl_easy_setopt(pCurl, CURLOPT_VERBOSE, 1L);	// Show debug info
-					curl_easy_setopt(pCurl, CURLOPT_URL, DEVICE_API_URL);
-					pHttpHeaders = NULL;
-					pHttpHeaders = curl_slist_append(pHttpHeaders, "Content-Type: application/json");
-					pHttpHeaders = curl_slist_append(pHttpHeaders, "api-key:PPsmfd=F0gDlwdZLgGsrVEAsIeY=");
-					curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, pHttpHeaders);
 
-					sprintf(cPostData, "{\"Temperature\":%.01f,\"Humidity\":%.01f,\"AirQuality\":%u}",
-							(double)(tSensorData.nTemperature) / 10,
-							(double)(tSensorData.unHumidity) / 10,
-							tSensorData.unAirQuality);
-					puts(cPostData);
+				sprintf(cPostData, "{\"Temperature\":%.01f,\"Humidity\":%.01f,\"AirQuality\":%u}",
+						(double)(tSensorData.nTemperature) / 10,
+						(double)(tSensorData.unHumidity) / 10,
+						tSensorData.unAirQuality);
+				puts(cPostData);
 
-					/* Now specify the POST data */
-					curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, cPostData);
+				/* Now specify the POST data */
+				curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, cPostData);
 
-					/* Perform the request, res will get the return code */
-					tCurlRslt = curl_easy_perform(pCurl);
-					/* Check for errors */
-					if(tCurlRslt != CURLE_OK){
-						fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(tCurlRslt));
-					}
-					/* always cleanup */
-					curl_easy_cleanup(pCurl);
+				/* Perform the request, res will get the return code */
+				tCurlRslt = curl_easy_perform(pCurl);
+				/* Check for errors */
+				if(tCurlRslt != CURLE_OK){
+					fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(tCurlRslt));
 				}
 			}
 		}
 
-		sleep(2);
+		sleep(5);
 	}
+	/* cleanup */
+	curl_slist_free_all(pHttpHeaders);
+	curl_easy_cleanup(pCurl);
 
 	close(nConnectedSocketFd);
 	return EXIT_SUCCESS;
